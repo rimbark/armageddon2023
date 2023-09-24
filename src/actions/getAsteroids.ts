037fa499702e90c06bf49598ab5fc2bf84190aba
Dzane <dzane@student.21-school.ru>
2023-09-24 17:19:59 +0700

    fit: добавлена типизация в функцию

--- src/actions/getAsteroids.ts
+++ src/actions/getAsteroids.ts
@@ -35,10 +35,9 @@ export async function getAsteroidById({ date, id }: IParams) {
     `https://api.nasa.gov/neo/rest/v1/feed?start_date=${START_DATE}&end_date=${date}&api_key=${API_KEY}`,
   )
   const data = await response?.json()
-  if (data) {
-    const asteroids: IAsteroidInfo[] = data.near_earth_objects[date]
+  const asteroids: IAsteroidInfo[] = data.near_earth_objects[date]
+  if (asteroids) {
     const asteroid: IAsteroidInfo = asteroids.find(item => item.id === id.toString())
-
     return asteroid
   } else {
     throw new Error('Asteroid not found')
