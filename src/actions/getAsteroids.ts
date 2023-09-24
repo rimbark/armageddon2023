9e960e0d329ff1bd3c3ece48c31f58ebc3cf56ad
Dzane <dzane@student.21-school.ru>
2023-09-24 15:02:34 +0700

    fit: добавлена типизация в функцию

--- src/actions/getAsteroids.ts
+++ src/actions/getAsteroids.ts
@@ -10,7 +10,6 @@ interface IParams {
 }
 
 export function getNextDayDate(date: string) {
-  console.log(date)
   const nextDay = new Date(date)
   nextDay.setDate(nextDay.getDate() + 1)
   return nextDay.toISOString().split('T')[0]
@@ -23,6 +22,7 @@ export async function getAsteroids(): Promise<IRoot> {
 }
 
 export async function getAsteroidsNextDay(requestDay): Promise<IRoot> {
+  console.log(requestDay)
   return await fetch(
     `https://api.nasa.gov/neo/rest/v1/feed?start_date=${requestDay}&end_date=${requestDay}&api_key=${API_KEY}`,
   ).then(resp => resp.json())
