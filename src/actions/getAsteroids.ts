b960807d6138ee7d119a06a1b3ba820905509fe0
Dzane <dzane@student.21-school.ru>
2023-09-24 14:58:47 +0700

    fit: добавлена типизация в функцию

--- src/actions/getAsteroids.ts
+++ src/actions/getAsteroids.ts
@@ -9,7 +9,8 @@ interface IParams {
   id: number
 }
 
-export function getNextDayDate(date) {
+export function getNextDayDate(date: string) {
+  console.log(date)
   const nextDay = new Date(date)
   nextDay.setDate(nextDay.getDate() + 1)
   return nextDay.toISOString().split('T')[0]
@@ -17,19 +18,19 @@ export function getNextDayDate(date) {
 
 export async function getAsteroids(): Promise<IRoot> {
   return await fetch(
-      `https://api.nasa.gov/neo/rest/v1/feed?start_date=${START_DATE}&end_date=${START_DATE}&api_key=${API_KEY}`,
+    `https://api.nasa.gov/neo/rest/v1/feed?start_date=${START_DATE}&end_date=${START_DATE}&api_key=${API_KEY}`,
   ).then(resp => resp.json())
 }
 
 export async function getAsteroidsNextDay(requestDay): Promise<IRoot> {
   return await fetch(
-      `https://api.nasa.gov/neo/rest/v1/feed?start_date=${requestDay}&end_date=${requestDay}&api_key=${API_KEY}`,
+    `https://api.nasa.gov/neo/rest/v1/feed?start_date=${requestDay}&end_date=${requestDay}&api_key=${API_KEY}`,
   ).then(resp => resp.json())
 }
 
-export async function getAsteroidById({date, id}: IParams) {
+export async function getAsteroidById({ date, id }: IParams) {
   const response = await fetch(
-      `https://api.nasa.gov/neo/rest/v1/feed?start_date=${START_DATE}&end_date=${date}&api_key=${API_KEY}`,
+    `https://api.nasa.gov/neo/rest/v1/feed?start_date=${START_DATE}&end_date=${date}&api_key=${API_KEY}`,
   )
   const data = await response.json()
   const asteroids: IAsteroidInfo[] = data.near_earth_objects[date]
