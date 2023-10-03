7ee4bce6f185576dda5737af26915f00389e57ea
Dzane <dzane@student.21-school.ru>
2023-10-03 20:18:51 +0700

    fit: добавлен переход на главную страницу при нажатии на заголовок ARMAGEDDON 2023

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,11 +2,14 @@ import cn from 'clsx'
 import React from 'react'
 import s from './style/Header.module.scss'
 import { passionOne } from '@/app/passionOne.constants'
+import Link from 'next/link'
 
 export const Header = () => {
   return (
     <header className={cn(s.container, passionOne.variable)}>
-      <h1>ARMAGEDDON 2023</h1>
+      <Link href={'/'} className={s.link}>
+        <h1>ARMAGEDDON 2023</h1>
+      </Link>
       <p>ООО “Команда им. Б. Уиллиса”. Взрываем астероиды с 1998 года.</p>
     </header>
   )
